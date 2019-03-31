f0351880d45f973013f5a28755b687935dac34f7
wozzp <williamcpereira89@gmail.com>
2019-03-31 11:52:24 -0300

    redux dev tools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,19 @@
-import { createStore /* , applyMiddleware */ } from 'redux';
+import { createStore /* , applyMiddleware */, compose } from 'redux';
 // import createSagaMiddleware from 'redux-saga';
 
 import ducks from './ducks';
 // import sagas from './sagas';
 
 // const sagaMiddleware = createSagaMiddleware();
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
 
-const store = createStore(ducks /*, applyMiddleware(sagaMiddleware) */);
+const store = createStore(
+	ducks
+	/*, applyMiddleware(sagaMiddleware) */,
+	compose(
+		devTools
+	)
+);
 
 // sagaMiddleware.run(sagas);
 
